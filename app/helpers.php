9626cb2554b5474a8f499f6b0b7d4a99b2f2c070
Masaki Kawabata Neto <kawabata@usp.br>
2021-06-29 16:38:17 -0300

    passando coisas pro config

--- app/helpers.php
+++ app/helpers.php
@@ -1,46 +1,28 @@
 <?php
 
-use League\CommonMark\Environment;
-use League\CommonMark\CommonMarkConverter;
-use League\CommonMark\Normalizer\SlugNormalizer;
 use League\CommonMark\Block\Renderer\FencedCodeRenderer;
 use League\CommonMark\Block\Renderer\IndentedCodeRenderer;
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment;
 use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
-use League\CommonMark\Extension\TableOfContents\TableOfContentsExtension;
 use League\CommonMark\Extension\HeadingPermalink\HeadingPermalinkExtension;
+use League\CommonMark\Extension\TableOfContents\TableOfContentsExtension;
+use League\CommonMark\Normalizer\SlugNormalizer;
 
-function markdown($markdown)
-{
+if (!function_exists('markdown')) {
+    function markdown($markdown)
+    {
+        $environment = Environment::createCommonMarkEnvironment();
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
 
-    $environment = Environment::createCommonMarkEnvironment();
-    $environment->addExtension(new GithubFlavoredMarkdownExtension());
+        $environment->addExtension(new HeadingPermalinkExtension());
+        $environment->addExtension(new TableOfContentsExtension());
 
-    $environment->addExtension(new HeadingPermalinkExtension());
-    $environment->addExtension(new TableOfContentsExtension());
+        # ainda sim precisei colocar o highlight.js. Tem de investigar mais
+        $environment->addBlockRenderer(FencedCode::class, new FencedCodeRenderer(config('app-export.highlight')));
+        $environment->addBlockRenderer(IndentedCode::class, new IndentedCodeRenderer(config('app-export.highlight')));
 
-    $environment->addBlockRenderer(FencedCode::class, new FencedCodeRenderer(['html', 'php', 'javascript', 'bash']));
-    $environment->addBlockRenderer(IndentedCode::class, new IndentedCodeRenderer(['html', 'php', 'javascript', 'bash']));
-
-    $config = [
-        'table_of_contents' => [
-            'html_class' => 'toc', //'table-of-contents',
-            'position' => 'placeholder',
-            'style' => 'bullet',
-            'min_heading_level' => 2,
-            'max_heading_level' => 6,
-            'normalize' => 'relative',
-            'placeholder' => '<ul id="toc"></ul>',
-        ],
-        'heading_permalink' => [
-            'html_class' => 'heading-permalink',
-            'id_prefix' => 'user-content',
-            'insert' => 'before',
-            'title' => 'Permalink',
-            'symbol' => '', //HeadingPermalinkRenderer::DEFAULT_SYMBOL,
-            'slug_normalizer' => new SlugNormalizer(),
-        ],
-    ];
-
-    $converter = new CommonMarkConverter($config, $environment);
-    return $converter->convertToHtml($markdown);
+        $converter = new CommonMarkConverter(config('app-export.commonmark'), $environment);
+        return $converter->convertToHtml($markdown);
+    }
 }
--- config/app-export.php
+++ config/app-export.php
@@ -2,4 +2,31 @@
 
 return [
     'baseurl' => env('EXPORT_BASEURL', null),
+
+    'commonmark' => [
+
+        # para commonmark, table of contents
+        'table_of_contents' => [
+            'html_class' => 'toc', //'table-of-contents',
+            'position' => 'placeholder',
+            'style' => 'bullet',
+            'min_heading_level' => 2,
+            'max_heading_level' => 6,
+            'normalize' => 'relative',
+            'placeholder' => '<ul id="toc"></ul>',
+        ],
+
+        # commonmark, permalinks
+        'heading_permalink' => [
+            'html_class' => 'heading-permalink',
+            'id_prefix' => 'user-content',
+            'insert' => 'before',
+            'title' => 'Permalink',
+            'symbol' => '', //HeadingPermalinkRenderer::DEFAULT_SYMBOL,
+           // 'slug_normalizer' => new SlugNormalizer(),
+        ],
+    ],
+
+    'highlight' => ['html', 'php', 'javascript', 'bash'],
+
 ];
